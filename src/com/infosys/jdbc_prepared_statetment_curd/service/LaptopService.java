d07872ca472c6664a563245dc81b0731c76d1d17
Mohsin <mdmohsinhaider6@gmail.com>
2024-11-22 02:20:18 +0530

    JDBC Service  Opration PreparedStatement interface

---
+++ src/com/infosys/jdbc_prepared_statetment_curd/service/LaptopService.java
@@ -0,0 +1,7 @@
+package com.infosys.jdbc_prepared_statetment_curd.service;
+
+import com.infosys.jdbc_prepared_statetment_curd.dao.LaptopDao;
+
+public class LaptopService {
+    LaptopDao dao = new LaptopDao();
+}
