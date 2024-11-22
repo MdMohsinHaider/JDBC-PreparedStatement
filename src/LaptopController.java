94cc6482adc8a906630fb312c63c3af6564263d1
Mohsin <mdmohsinhaider6@gmail.com>
2024-11-22 23:58:52 +0530

    Laptop Controller

--- src/LaptopController.java
+++ src/LaptopController.java
@@ -9,93 +9,102 @@ import java.util.Scanner;
 public class LaptopController {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("1. Insert\n2. Delete\n3. Update\n4. Display \n5. Display by Id\n6. Laptop Filter by price");
-        System.out.println("Please Enter Choice");
-        int choice = scanner.nextInt();
+        char ch;
 
-        LaptopDao dao = new LaptopDao();
-        LaptopService laptopService = new LaptopService();
+        while (true){
+            System.out.println("1. Insert\n2. Delete\n3. Update\n4. Display \n5. Display by Id\n6. Laptop Filter by price");
+            System.out.println("Please Enter Choice");
+            int choice = scanner.nextInt();
 
-        switch (choice) {
-            case 1: {
-                System.out.println("Enter Laptop id : ");
-                int id = scanner.nextInt();
-                System.out.println("Enter Laptop name : ");
-                String name = scanner.next();
-                System.out.println("Enter Laptop color : ");
-                String color = scanner.next();
-                System.out.println("Enter Laptop price : ");
-                double price = scanner.nextDouble();
-                // pass mfd (yyy-mm-dd)
-                System.out.println("Enter laptop manufacturing date : ");
-                String mfd = scanner.next();
+            LaptopDao dao = new LaptopDao();
+            LaptopService laptopService = new LaptopService();
+
+            switch (choice) {
+                case 1: {
+                    System.out.println("Enter Laptop id : ");
+                    int id = scanner.nextInt();
+                    System.out.println("Enter Laptop name : ");
+                    String name = scanner.next();
+                    System.out.println("Enter Laptop color : ");
+                    String color = scanner.next();
+                    System.out.println("Enter Laptop price : ");
+                    double price = scanner.nextDouble();
+                    // pass mfd (yyy-mm-dd)
+                    System.out.println("Enter laptop manufacturing date : ");
+                    String mfd = scanner.next();
 
 //				LocalDate date = LocalDate.parse(mfd);
 //				Laptop laptop = new Laptop(id,name,color,price,LocalDate.parse(mfd));
-                Laptop laptop = laptopService.saveLaptopService(new Laptop(id,name,color,price, LocalDate.parse(mfd)));
+                    Laptop laptop = laptopService.saveLaptopService(new Laptop(id,name,color,price, LocalDate.parse(mfd)));
 
-                String msg = laptop!=null?"Successfully data Store":"data not store";
-                System.out.println(msg);
+                    String msg = laptop!=null?"Successfully data Store":"data not store";
+                    System.out.println(msg);
 
-            }break;
-            case 2: {
-                System.out.println("Enter laptop Id");
-                int id = scanner.nextInt();
-                int a = dao.deleteLaptopByIdDao(id);
-                String msgString = a!=0?"deleted":"given id is not present";
-                System.out.println(msgString);
-            }break;
-            case 3:{
-                System.out.print("Enter laptop Id : ");
-                int id = scanner.nextInt();
+                }break;
+                case 2: {
+                    System.out.println("Enter laptop Id");
+                    int id = scanner.nextInt();
+                    int a = dao.deleteLaptopByIdDao(id);
+                    String msgString = a!=0?"deleted":"given id is not present";
+                    System.out.println(msgString);
+                }break;
+                case 3:{
+                    System.out.print("Enter laptop Id : ");
+                    int id = scanner.nextInt();
 
-                // take input from user
-                System.out.println("Enter Laptop name : ");
-                String name = scanner.next();
-                System.out.println("Enter Laptop color : ");
-                String color = scanner.next();
-                System.out.println("Enter Laptop price : ");
-                double price = scanner.nextDouble();
-                // pass mfd (yyy-mm-dd)
-                System.out.println("Enter laptop manufacturing date : ");
-                String mfd = scanner.next();
+                    // take input from user
+                    System.out.println("Enter Laptop name : ");
+                    String name = scanner.next();
+                    System.out.println("Enter Laptop color : ");
+                    String color = scanner.next();
+                    System.out.println("Enter Laptop price : ");
+                    double price = scanner.nextDouble();
+                    // pass mfd (yyy-mm-dd)
+                    System.out.println("Enter laptop manufacturing date : ");
+                    String mfd = scanner.next();
 
-                // create a laptop object and set the desire value or filed
-                Laptop laptop = new Laptop();
-                laptop.setName(name);
-                laptop.setColor(color);
-                laptop.setPrice(price);
-                laptop.setMfd(LocalDate.parse(mfd));
+                    // create a laptop object and set the desire value or filed
+                    Laptop laptop = new Laptop();
+                    laptop.setName(name);
+                    laptop.setColor(color);
+                    laptop.setPrice(price);
+                    laptop.setMfd(LocalDate.parse(mfd));
 
-                Laptop laptop1 = dao.updateLaptopByIdDao(id,laptop);
-                System.out.println(laptop1);
+                    Laptop laptop1 = dao.updateLaptopByIdDao(id,laptop);
+                    System.out.println(laptop1);
 
-            }break;
-            case 4: {
-                List<Laptop> laptops = dao.getAllLaptopDao();
-                for(Laptop laptop:laptops){
-                    System.out.println(laptop);
-                }
-            }break;
-            case 5: {
-                System.out.print("Enter laptop id to find laptop : ");
-                int id = scanner.nextInt();
-                Laptop laptop = dao.getLaptopByIdDao(id);
-                System.out.println(laptop);
-            }break;
-            case 6:{
-                System.out.print("Enter laptop price which is less than : ");
-                double user_filter_price = scanner.nextDouble();
-                List<Laptop> laptops = laptopService.filterLaptopByPriceService(user_filter_price);
-                if (laptops.isEmpty()){
-                    System.out.println("Item Not Found");
-                }
-                else {
-                    for (Laptop laptop: laptops){
+                }break;
+                case 4: {
+                    List<Laptop> laptops = dao.getAllLaptopDao();
+                    for(Laptop laptop:laptops){
                         System.out.println(laptop);
+                    }
+                }break;
+                case 5: {
+                    System.out.print("Enter laptop id to find laptop : ");
+                    int id = scanner.nextInt();
+                    Laptop laptop = dao.getLaptopByIdDao(id);
+                    System.out.println(laptop);
+                }break;
+                case 6:{
+                    System.out.print("Enter laptop price which is less than : ");
+                    double user_filter_price = scanner.nextDouble();
+                    List<Laptop> laptops = laptopService.filterLaptopByPriceService(user_filter_price);
+                    if (laptops.isEmpty()){
+                        System.out.println("Item Not Found");
+                    }
+                    else {
+                        for (Laptop laptop: laptops){
+                            System.out.println(laptop);
+                        }
                     }
                 }
             }
+
+            // for continuity
+            System.out.print("Are You continue Y or y :  ");
+            ch = scanner.next().charAt(0);
+            if (!(ch == 'y' || ch == 'Y')) break;
         }
         scanner.close();
     }
